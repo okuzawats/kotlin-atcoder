2da52b7ee5247503142e70fff1a0762859bdf0a2
okuzawats <okuzawats@gmail.com>
2022-02-17 17:58:24 +0900

    [AC]Educational DP Contest A(refactoring)

--- src/main/kotlin/educational_dp/A.kt
+++ src/main/kotlin/educational_dp/A.kt
@@ -7,10 +7,11 @@ fun main() {
   val h = readLine()!!.split(' ').map(String::toInt)
 
   val c = IntArray(n)
+  val diff = { i: Int, j: Int -> abs(h[i] - h[j]) }
+  val cal = { i: Int -> min(c[i - 1] + diff(i, i - 1), c[i - 2] + diff(i, i - 2)) }
+
   c[0] = 0
-  c[1] = c[0] + abs(h[1] - h[0])
-  (2 until n).forEach { m ->
-    c[m] = min(c[m - 1] + abs(h[m] - h[m - 1]), c[m - 2] + abs(h[m] - h[m - 2]))
-  }
+  c[1] = c[0] + diff(1, 0)
+  (2 until n).forEach { c[it] = cal(it) }
   println(c[n - 1])
 }
