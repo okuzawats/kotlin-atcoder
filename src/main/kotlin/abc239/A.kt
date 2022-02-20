b138228f1113d6a61cbe9cac80069b478e684444
okuzawats <okuzawats@gmail.com>
2022-02-20 11:26:28 +0900

    [AC]ABC239A

--- src/main/kotlin/abc239/A.kt
+++ src/main/kotlin/abc239/A.kt
@@ -2,4 +2,4 @@ package abc239
 
 import kotlin.math.sqrt
 
-fun main() = readLine()!!.toDouble().let { println("${sqrt((it * (12800000 + it)))}") }
+fun main() = readLine()!!.toDouble().let { println(sqrt((it * (12800000 + it)))) }
