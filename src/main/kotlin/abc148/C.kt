079fcf27d148e6c2dbca2d7a3872d406a33ab636
okuzawats <okuzawats@gmail.com>
2022-02-23 22:28:02 +0900

    [AC]ABC148C

--- src/main/kotlin/abc148/C.kt
+++ src/main/kotlin/abc148/C.kt
@@ -1,12 +1,8 @@
 package abc148
 
-tailrec fun gcd(a: Long, b: Long): Long =
-  if (b == 0L) a
-  else gcd(b, a % b)
-
-fun lcm(a: Long, b: Long) = a / gcd(a, b) * b
+import java.math.*
 
 fun main() {
-  val (a, b) = readLine()!!.split(' ').map(String::toLong)
-  println(lcm(a, b))
+  val (a, b) = readLine()!!.split(' ').map(::BigInteger)
+  println(a / a.gcd(b) * b)
 }
