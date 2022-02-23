c420025a95386605a9e98bc64cc6d0f8340bfa2f
okuzawats <okuzawats@gmail.com>
2022-02-23 09:38:36 +0900

    [AC]ABC235B

---
+++ src/main/kotlin/abc235/B.kt
@@ -0,0 +1,9 @@
+package abc235
+
+fun main() {
+  val n = readLine()!!.toInt()
+  val h = readLine()!!.split(' ').map(String::toInt) + -1
+  (0 .. n)
+    .first { h[it + 1] <= h[it] }
+    .let { println(h[it]) }
+}
